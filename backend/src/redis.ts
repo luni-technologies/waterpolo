2655ed29a1e248f7cfe24b37ce2e4396f14df547
Igor Zimmermann <igorzimmermann01@gmail.com>
2023-04-07 22:10:38 +0200

    [ADD] Add redis connection options

--- backend/src/redis.ts
+++ backend/src/redis.ts
@@ -1,3 +1,9 @@
 import Redis from 'ioredis'
 
-export const redis = new Redis()
+export const redis = new Redis({
+	port: parseInt(process.env.REDIS_PORT || '6379'),
+	host: process.env.REDIS_HOST,
+	username: process.env.REDIS_USER,
+	password: process.env.REDIS_PASS,
+	db: 0,
+})
